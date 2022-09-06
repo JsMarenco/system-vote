685411243ead602856e789762b4cb0ed054c343f
Angel Marenco <jsmarenco.dev@gmail.com>
2022-09-06 15:02:48 -0600

    ForgotPassword page was add

---
+++ src/pages/forgot-password/index.tsx
@@ -0,0 +1,65 @@
+import { Box, Typography, TextField, Button } from '@mui/material'
+import { containedButtonStyle } from '../../styles/buttons'
+import { containerV1, mainContainer } from '../../styles/containers'
+
+export default function ForgotPassword() {
+  return (
+    <>
+      <Box sx={mainContainer}>
+        <Box sx={containerV1} component="form" id="forgot-password-form">
+          <Typography
+            variant="h5"
+            color="text.primary"
+            sx={{ fontWeight: 700 }}
+            align="center"
+          >
+            Enter your email
+          </Typography>
+
+          <TextField
+            name="email"
+            label="Email"
+            type="email"
+            variant="outlined"
+            fullWidth
+            required
+            sx={{
+              my: 2,
+            }}
+          />
+
+          <Typography
+            variant="body1"
+            color="text.primary"
+            align="center"
+          >
+            We will send you a link to reset your password
+          </Typography>
+
+          <Typography
+            variant="body1"
+            color="text.primary"
+            align="center"
+          >
+            {'If you don\'t receive an email, please check your spam folder'}
+          </Typography>
+
+          <Button
+            variant="text"
+            color="primary"
+            sx={{
+              ...containedButtonStyle,
+              mt: 2,
+              mx: 'auto',
+              display: 'block',
+              textAlign: 'center',
+            }}
+            href="/reset-password"
+          >
+            Send
+          </Button>
+        </Box>
+      </Box>
+    </>
+  )
+}
