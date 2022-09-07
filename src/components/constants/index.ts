fcfe527303ee8e1e840f5b9df8b0d5c517dd81b0
Angel Marenco <jsmarenco.dev@gmail.com>
2022-09-06 23:24:51 -0600

    constants were add

---
+++ src/components/constants/index.ts
@@ -0,0 +1,15 @@
+export const REGISTER_PAGE_DESCRIPTION = 'This is the register page, where you can register for an account. You can also login here if you already have an account.'
+
+export const PAGE_NOT_FOUND_PAGE_DESCRIPTION = 'This is the page not found page, where you can see that the page you are looking for does not exist.'
+
+export const LOGIN_PAGE_DESCRIPTION = 'This is the login page, where you can login to your account. You can also register here if you do not have an account.'
+
+export const DASHBOARD_PAGE_DESCRIPTION = 'This is the dashboard page, where you can see the latest posts.'
+
+export const CREATE_VOTATION_PAGE_DESCRIPTION = 'This is the create votation page, where you can create a votation.'
+
+export const FORGOT_PASSWORD_PAGE_DESCRIPTION = 'This is the forgot password page, where you can reset your password.'
+
+export const RESET_PASSWORD_PAGE_DESCRIPTION = 'This is the reset password page, where you can reset your password.'
+
+export const MY_VOTATIONS_PAGE_DESCRIPTION = 'Here you will find all your votations. You can also create a new votation. You can also see the votations you have voted on.'
